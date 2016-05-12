28d2d68b8274c64a17ffde653fa4db13861234ca
farbton <farbton@gmx.net>
2016-05-12 08:06:19 +0200

    fkt. calculate erweitert
    Die erste kleine Berechnung wird gemacht. Aber nur mit festen Werten.

--- HelloWorld/app/src/main/java/com/example/kirko/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/kirko/helloworld/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
@@ -14,10 +15,15 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
     int counter = 0;
+    private static final String TAG = "MyActivity";
+    //DecimalFormat
+    DecimalFormat df = new DecimalFormat("#0.000");
 
 
     @Override
@@ -68,18 +74,35 @@ public class MainActivity extends AppCompatActivity {
         tview.setText(String.valueOf(counter));
     }
     public void calculate(View view){
+
+        double alpha = 0.0167;//in Grad=1 winkelminute
+
         TextView zollview = (TextView) findViewById(R.id.zoll);
-        zollview.setText("28");
+        //zollview.setText("72");
         zollview.setGravity(Gravity.CENTER_HORIZONTAL);
 
         TextView distanceview = (TextView) findViewById(R.id.distance);
-        distanceview.setText("2,3");
+       // distanceview.setText("2,3");
         distanceview.setGravity(Gravity.CENTER_HORIZONTAL);
 
         TextView resolutionview = (TextView) findViewById(R.id.resolution);
-        resolutionview.setText("1080x1920");
+        resolutionview.setText("575");
         resolutionview.setGravity(Gravity.CENTER_HORIZONTAL);
 
+        double relation = 4/3;
+        double high     = Math.sqrt((Math.pow(Integer.valueOf(zollview.getText().toString()),2)  * 9 ) / 25);
+        double width    = relation * high;
 
+        double row_high = high / Integer.valueOf(resolutionview.getText().toString());
+
+        double distance = row_high / (100 * Math.tan(Math.toRadians(alpha)));
+
+
+        distanceview.setText(String.valueOf(df.format(distance)));
+        System.out.print(high + width + row_high + distance);
+
+        Log.i(TAG,String.valueOf(Math.tan(Math.toRadians(alpha))));
     }
+
+
 }
